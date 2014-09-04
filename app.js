b323258ff8307877d2824c578dad99671c2143fa
stac <ehsanmasdar@gmail.com>
2014-09-04 09:26:21 -0500

    Sockets.io

--- app.js
+++ app.js
@@ -1,59 +1,22 @@
+// Setup basic express server
 var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = 3001;
+app.use("/styles", express.static(__dirname + '/styles'));
+// Routing
+app.get('/', function(req, res){
+  res.sendfile('index.html');
 });
 
-/// error handlers
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+io.on('connection', function (socket) {
+  socket.emit('hello', { hello: "test"});
 });
-
-
-module.exports = app;
+http.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
