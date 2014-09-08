4576fbf64fa76a12cbd0ebf5219f226f2a20151f
stac <ehsanmasdar@gmail.com>
2014-09-08 12:59:07 -0500

    Switch to a room based system

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = 3001;
+
+
 app.use("/styles", express.static(__dirname + '/styles'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/static", express.static(__dirname + '/static'));
 // Routing
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
+
 var currentusers = 0;
 var totalusers = 0;
 var users = [];
+var rooms = 0;
+//read in file names
+var fs = require('fs');
+var roomnames = fs.readFileSync('./static/roomnames.txt').toString().split("\n");
 io.on('connection', function (socket) {
   var id = totalusers;
   totalusers++;
@@ -22,6 +31,7 @@ io.on('connection', function (socket) {
   socket.on('reply', function(data){
         socket.emit('hello', merge_options(data,{"server": "reply"}));
     });
+
   socket.on('join', function () {
     console.log( 'user ' + id + '  requested join');
     //Set socket sate to searching
@@ -31,33 +41,52 @@ io.on('connection', function (socket) {
       if (entry.searching == true && entry != socket){
         //Match found, set searching to false and join to true
         socket.searching = false;
+
         entry.searching = false;
-        socket.join = true;
-        entry.join = true;
-        //Tell clients who they are connected to
-        //I represents the socket the local socket connects to
-        socket.connectedTo = i;
-        //id represents the number of the local socket that the external connected to 
-        entry.connectedTo = id;
+        socket.isConnected = true;
+        entry.isConnected = true;
+        //Tell clients what room they are connected to
+        //Room is a random name
+        var room = roomnames[rooms];
+        rooms++;
+        socket.connectedToRoom = room;
+        entry.connectedToRoom = room;
         //Finally, give each player a turn order number (0 for first, 1 for second)
         entry.player = 0;
         socket.player = 1;
-
-        socket.emit("connection", {"id":i});
-        entry.emit("connection", {"id":id});
-        console.log("Player id " + i + " successfully connected to player " + id);
+        socket.join(room);
+        entry.join(room);
+        socket.emit('connection', {"id":room});
+        entry.emit('connection', {"id":room});
+        console.log("Player id " + i + " successfully connected to player " + id + " at room " + room);
       }
     }
   });
+
+  socket.on('gamesend', function (data) {
+    if (socket.isConnected){
+      console.log(data);
+      //To just other clients
+      //socket.to(socket.connectedToRoom).emit('gamerecieve', data)
+      //To everyone
+      io.sockets.in(socket.connectedToRoom).emit('gamerecieve', data);
+    } 
+  });
+
   socket.on('disconnect', function () {
     console.log( 'user ' + id + '  disconnected');
     currentusers--;
     console.log("Curent Users: " + currentusers);
   });
+
 });
+
+
 http.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
+
+
 function merge_options(obj1,obj2){
     var obj3 = {};
     for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
---
+++ js/client.js
@@ -0,0 +1,25 @@
+var socket = io();
+
+socket.on('hello', function (data) {
+  console.log('hello:', data);
+  $('#id').text(function(i, oldText) {
+     return data.id;
+    });
+});
+
+socket.on('gamerecieve', function (data) {
+  console.log('gamerecieve:', data);
+});
+
+socket.on('connection', function (data) {
+  console.log('connection:', data);
+  $('#connection').text(function(i, oldText) {
+    return "Connected to room "  + data.id;
+  });
+});
+$("#join").submit(function(event)
+{
+  event.preventDefault(); // disable normal form submit behavior
+  socket.emit('join');
+  return false; // prevent further bubbling of event
+});
