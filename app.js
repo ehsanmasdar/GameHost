7983dc61724117d7549fdc6766e041ee05d90e90
Ehsan Asdar <ehsanmasdar@gmail.com>
2014-09-19 09:43:20 -0500

    Work on registration

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ var _ = require('lodash');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var room = require('./room.js');
+var uuid = require('node-uuid');
 var port = 3002;
-
 // Routing
+app.engine('html', require('ejs').renderFile);
 app.use("/styles", express.static(__dirname + '/styles'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/static", express.static(__dirname + '/static'));
 app.get('/', function(req, res){
-    res.sendfile('index.html');
+    res.render('index.html');
+});
+app.get('/register', function(req, res){
+	var newid = uuid.v1();
+	console.log(newid);
+    res.render('register.html', {id:newid});
 });
 
 io.on('connection', _.partial(room.onConnection, io));
---
+++ users.js
@@ -0,0 +1,2 @@
+var _ = require('lodash');
+var uuid = require('node-uuid');
