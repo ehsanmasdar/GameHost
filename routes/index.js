2ec4df96b88373e0ebb674f381528134b905bea9
Ehsan Asdar <ehsanmasdar@gmail.com>
2014-10-01 13:09:17 -0500

    Mongodb connection and storage of ids (but not names yet)

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var uuid = require('node-uuid');
 var router = express.Router();
+//Mongo DB setup
+var mongoose = require('mongoose');
+var mongosettings = {
+  user: 'sam',
+  pass: ''
+}
+var schema = new mongoose.Schema({ name: 'string', id: 'string' });
+var user = mongoose.model('user', schema);
+mongoose.connect("ehsandev.com/GameHost",mongosettings)
 /* GET home page. */
 router.get('/', function(req, res) {
     res.render('index.html');
 });
 router.get('/register', function(req, res) {
 	var newid = uuid.v1();
+	var username = "Test";
+	var useToRegister = new user({name:username,id:newid})
+	useToRegister.save(function (err) {
+  	if (err) return handleError(err);
+  		// saved!
+	})
 	console.log(newid);
-    res.render('register.html', {id:  "blah"});
+    res.render('register.jade', {id:  newid});
 });
 module.exports = router;
